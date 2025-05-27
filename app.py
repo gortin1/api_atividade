68ddfcda61bff5c1a7c995a1b86f74d2306bcb8c
Camila Ribeiro <134373868+camilasribeiro@users.noreply.github.com>
2025-05-26 23:59:58 -0300

    Arquivo principal para execução da aplicação

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from database import db
-from api.reserva.reserva_route import reservas_blueprint
+from api.atividade.atividade_route import atividades_blueprint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atividades.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-app.register_blueprint(reservas_blueprint)
+app.register_blueprint(atividades_blueprint)
 
 
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
